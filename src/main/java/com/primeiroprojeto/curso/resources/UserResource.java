d276446506ec51d2a8d9a93ad29712174334fe7d
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-02-08 16:09:49 -0300

    user entity e resources

--- src/main/java/com/primeiroprojeto/curso/resources/UserResource.java
+++ src/main/java/com/primeiroprojeto/curso/resources/UserResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
     @GetMapping
     public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+        User user = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
         return ResponseEntity.ok().body(user);
     }
 }
