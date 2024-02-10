471ae29f942e04b670634ee6156155e1878e780a
VIMMMMMMM <du.rocha.santos@gmail.com>
2024-02-09 22:55:23 -0300

    OrderStatus enum

--- src/main/java/com/primeiroprojeto/curso/entities/Order.java
+++ src/main/java/com/primeiroprojeto/curso/entities/Order.java
@@ -1,6 +1,7 @@
 package com.primeiroprojeto.curso.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.primeiroprojeto.curso.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,16 +15,29 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
-public Order(){
 
-}
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus ,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus!=null)
+        this.orderStatus = orderStatus.getCode();
+    }
+    public Order(){
+
     }
 
     public Long getId() {
