c7008c6c806502f69a1879928ac18e15baccc6b6
VIMMMMMMM <du.rocha.santos@gmail.com>
2024-02-12 17:46:38 -0300

    adicionando tratamento de excecao para o metodo delete

--- src/main/java/com/primeiroprojeto/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/primeiroprojeto/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.primeiroprojeto.curso.resources.exceptions;
 
+import com.primeiroprojeto.curso.services.exceptions.DatabaseException;
 import com.primeiroprojeto.curso.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,13 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error= "Resource not found";
         HttpStatus status= HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request){
+        String error= "Database error";
+        HttpStatus status= HttpStatus.BAD_REQUEST;
         StandardError standardError = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
     }
---
+++ src/main/java/com/primeiroprojeto/curso/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.primeiroprojeto.curso.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
